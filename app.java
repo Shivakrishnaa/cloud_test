4b6380a84f48c6adb4cd7f86eae178a5cc8c135d
unknown <shivakrishnaerumalla@gmail.com>
2026-03-24 15:03:00 +0530

    update by anudeep

--- app.java
+++ app.java
@@ -228,3 +228,24 @@ pipeline {
   }
 }
 This directory contains examples of how to extend Workflow using the global library on your Jenkins master.
+pipeline {
+  agent {
+    dockerfile {
+      /*
+        * The Default is "Dockerfile" but this can be changed.
+        * This will build a new container based on the contents of "Dockerfile.alternate"
+        * and run the pipline inside this container
+        */
+      filename "Dockerfile.alternate"
+      args "-v /tmp:/tmp -p 8000:8000"
+    }
+  }
+  stages {
+    stage("foo") {
+      steps {
+        sh 'cat /hi-there'
+        sh 'echo "The answer is 42"'
+      }
+    }
+  }
+}
