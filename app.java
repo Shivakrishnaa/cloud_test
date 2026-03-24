50ce9e65b35823937fa42146e06be75cce7591b0
unknown <shivakrishnaerumalla@gmail.com>
2026-03-24 13:27:14 +0530

    sample jenkins

--- app.java
+++ app.java
@@ -1,10 +1,19 @@
-public class app
-{
-    hello shiva krishna
-    hello vishnu
-    hello kranthi 
-    hello anudeep
-    hello venkat
-    hello vijay
-    ok for this 
-}
+agent {
+    dockerfile {
+      /*
+        * The Default is "Dockerfile" but this can be changed.
+        * This will build a new container based on the contents of "Dockerfile.alternate"
+        * and run the pipline inside this container
+        */
+      filename "Dockerfile.alternate"
+      args "-v /tmp:/tmp -p 8000:8000"
+    }
+  }
+  stages {
+    stage("foo") {
+      steps {
+        sh 'cat /hi-there'
+        sh 'echo "The answer is 42"'
+      }
+    }
+  }
